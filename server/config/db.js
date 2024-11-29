2ddfb604da9a076cf063e1e80b5a1d373b08bb8f
@alexandru.predescu2017 <alexandru.predescu2017@gmail.com>
2024-11-29 15:17:11 +0200

    added .env + schimbat port

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const dbConnection = new Sequelize("sts-voting-app", "root", "", {
+dotenv.config();
+
+const dbConnection = new Sequelize("sts-voting-app", process.env.MYSQL, process.env.PASSWORD, {
 	host: "localhost",
 	dialect: "mysql",
 	define: {
